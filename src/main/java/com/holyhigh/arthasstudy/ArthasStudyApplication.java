1797890927c4b6f433842eb408614437cc89ca9b
HolyHigh <845803085@qq.com>
2022-01-10 22:18:11 +0800

    feat: Arthas工具学习，项目初始化

---
+++ src/main/java/com/holyhigh/arthasstudy/ArthasStudyApplication.java
@@ -0,0 +1,13 @@
+package com.holyhigh.arthasstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthasStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArthasStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holyhigh/arthasstudy/ArthasStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holyhigh.arthasstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthasStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
